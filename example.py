bcf967746b6200c04a46d7a972f6bcd62f51475f
delsin <1749970370@qq.com>
2025-09-03 10:18:34 -0400

    update observation (example.py adds rkv modes)

--- example.py
+++ example.py
@@ -13,6 +13,7 @@ from utils.apply_chat_template import apply_chat_template
 
 import json
 
+from rkv.monkeypatch import replace_llama, replace_qwen2
 
 # "/home/yangx/DeepSeek-R1-Distill-Qwen-7B"
 # "/home/yangx/QwQ-32B"
@@ -33,6 +34,9 @@ import json
 
 def gen_example(model_path: str = "deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
             rpc: bool = False,
+            rkv: bool = False,
+            rkv_mode: str = "rkv", # rkv, snapkv, h2o, streamingllm
+            rkv_budget: int = "4096",
             max_new_tokens: int = 32768,
             # RPC arguments
             P=4096,
@@ -44,6 +48,7 @@ def gen_example(model_path: str = "deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
             budget_ans=1024,
             cp_ratio=4.0,
             mode=None,
+            save_output: bool = False
             ):
 
     attn_implementation = 'eager'
@@ -57,37 +62,102 @@ def gen_example(model_path: str = "deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
     if rpc:
         enable_rpc(mode)
     
-    if "qwen" in model_path.lower() or "qwq" in model_path.lower():
-        from rpc.qwen2.qwen2_config import Qwen2Config
+    if rkv:
+        # ====== build compression config ======
+        compression_config = {
+            "method": rkv_mode,
+            "method_config": {
+                "budget": rkv_budget,
+                "window_size": 8,
+                "mix_lambda": 0.07,
+                "retain_ratio": 0.2,
+                "retain_direction": "last",
+                "first_tokens": 4,
+            },
+            "compression": None,
+            "update_kv": True
+        }
+        model_config = {
+            "divide_method": "step_length",
+            "divide_length": 128,
+            "compression_content": "think",
+        }
 
-        config = Qwen2Config.from_pretrained(model_path)
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_path, use_fast=True, padding_side="left"
+        )
 
-        config.update({'mode':mode})
+        if tokenizer.pad_token is None:
+            tokenizer.pad_token = tokenizer.eos_token
+            tokenizer.pad_token_id = tokenizer.eos_token_id
 
-        model = Qwen2ForCausalLM.from_pretrained(
+        # apply monkey patch
+        if "llama" in model_path.lower():
+            replace_llama(compression_config)
+        elif "qwen" in model_path.lower():
+            replace_qwen2(compression_config)
+        else:
+            raise ValueError(f"Unsupported model: {model_path}")
+
+        model = AutoModelForCausalLM.from_pretrained(
             model_path,
             torch_dtype=torch.bfloat16,
-            device_map="auto",
             low_cpu_mem_usage=True,
+            use_cache=True,
             attn_implementation=attn_implementation,
-            config=config
+            device_map="auto"
         )
-    elif "llama" in model_path.lower():
-        from rpc.llama.llama_config import LlamaConfig
+        model.eval()
 
-        config = LlamaConfig.from_pretrained(model_path)
+        model.config.update(model_config)
 
-        config.update({'mode':mode})
+        model.newline_token_ids = [
+            tokenizer.encode("\n")[-1],
+            tokenizer.encode(".\n")[-1],
+            tokenizer.encode(")\n")[-1],
+            tokenizer.encode("\n\n")[-1],
+            tokenizer.encode(".\n\n")[-1],
+            tokenizer.encode(")\n\n")[-1],
+        ]
 
-        model = LlamaForCausalLM.from_pretrained(
-            model_path,
-            torch_dtype=torch.bfloat16,
-            device_map="auto",
-            low_cpu_mem_usage=True,
-            attn_implementation=attn_implementation,
-            config=config
-        )
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
+        model.after_think_token_ids = [
+            tokenizer.encode("</think>")[-1],
+        ]
+
+    else:
+
+        if "qwen" in model_path.lower() or "qwq" in model_path.lower():
+            from rpc.qwen2.qwen2_config import Qwen2Config
+
+            config = Qwen2Config.from_pretrained(model_path)
+
+            config.update({'mode':mode})
+
+            model = Qwen2ForCausalLM.from_pretrained(
+                model_path,
+                torch_dtype=torch.bfloat16,
+                device_map="auto",
+                low_cpu_mem_usage=True,
+                attn_implementation=attn_implementation,
+                config=config
+            )
+        elif "llama" in model_path.lower():
+            from rpc.llama.llama_config import LlamaConfig
+
+            config = LlamaConfig.from_pretrained(model_path)
+
+            config.update({'mode':mode})
+
+            model = LlamaForCausalLM.from_pretrained(
+                model_path,
+                torch_dtype=torch.bfloat16,
+                device_map="auto",
+                low_cpu_mem_usage=True,
+                attn_implementation=attn_implementation,
+                config=config
+            )
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+    
     streamer = TextStreamer(tokenizer)
 
     if rpc:
@@ -103,6 +173,8 @@ def gen_example(model_path: str = "deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
                             budget_ans=budget_ans,
                             cp_ratio=cp_ratio
                             )
+    elif rkv:
+        print([f"RKV Inference -- {rkv_mode} Mode"])
     else:
         print(["Full KV Cache Inference"])
 
@@ -127,16 +199,18 @@ def gen_example(model_path: str = "deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
     output_length = outputs[0][context_length:].shape[-1]
     decoded_output = tokenizer.decode(outputs[0][context_length:], skip_special_tokens=True)
 
-    # Create data dictionary
-    data = {
-        "context_length": context_length,
-        "output_length": output_length,
-        "decoded_output": decoded_output
-    }
 
-    # Save to JSONL file
-    with open("/home/yangx/ReasoningPathCompression/observation/output.jsonl", "w", encoding="utf-8") as f:
-        f.write(json.dumps(data, ensure_ascii=False) + "\n")
+    if save_output:
+        # Create data dictionary
+        data = {
+            "context_length": context_length,
+            "output_length": output_length,
+            "decoded_output": decoded_output
+        }
+
+        # Save to JSONL file
+        with open("/home/yangx/ReasoningPathCompression/observation/output.jsonl", "w", encoding="utf-8") as f:
+            f.write(json.dumps(data, ensure_ascii=False) + "\n")
 
     print(f"\nContext Length: {context_length}")
     print(f"Output Length: {output_length}\n")
--- rkv/monkeypatch.py
+++ rkv/monkeypatch.py
@@ -13,8 +13,10 @@ from transformers.models.qwen2 import modeling_qwen2
 from .modeling import (
     LlamaAttention_init,
     LlamaAttention_forward,
+    LlamaVanillaAttention_forward,
     Qwen2Attention_init,
     Qwen2Attention_forward,
+    Qwen2VanillaAttention_forward,
     CausalLM_forward,
 )
 
@@ -24,6 +26,7 @@ def replace_llama(compression_config):
 
     modeling_llama.LlamaAttention.__init__ = init_wrapper
     modeling_llama.LlamaFlashAttention2.forward = LlamaAttention_forward
+    modeling_llama.LlamaAttention.forward = LlamaVanillaAttention_forward
     modeling_llama.LlamaForCausalLM.forward = CausalLM_forward
 
 
@@ -33,6 +36,7 @@ def replace_qwen2(compression_config):
 
     modeling_qwen2.Qwen2Attention.__init__ = init_wrapper
     modeling_qwen2.Qwen2FlashAttention2.forward = Qwen2Attention_forward
+    modeling_qwen2.Qwen2Attention.forward = Qwen2VanillaAttention_forward
     modeling_qwen2.Qwen2ForCausalLM.forward = CausalLM_forward
 
 # def replace_qwen3(compression_config):
