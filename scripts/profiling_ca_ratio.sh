e0a7b7f33d49cf718100589b1790dd2a2f53c123
delsin <1749970370@qq.com>
2025-09-19 08:33:28 -0400

    update shells

--- scripts/profiling_ca_ratio.sh
+++ scripts/profiling_ca_ratio.sh
@@ -26,16 +26,16 @@ BSZ=1
 #         --data_path "/home/yangx/ReasoningPathCompression/eval/outputs/$MODEL_NICKNAME/profile_ca_ratio/aime24_n1_full.jsonl" \
 #         --model $MODEL_NICKNAME --model_path $MODEL
 
-# CUDA_VISIBLE_DEVICES=${GPU_DEVICES} python -m eval.generate_answers.infer_hf \
-#         --data_path "/home/yangx/ReasoningPathCompression/datasets/gsm8k" \
-#         --output_file "eval/outputs/$MODEL_NICKNAME/profile_ca_ratio/gsm8k_n1_full.jsonl" \
-#         --n_samples $N_SAMPLES \
-#         --batch_size $BSZ \
-#         --model_path $MODEL 
+CUDA_VISIBLE_DEVICES=${GPU_DEVICES} python -m eval.generate_answers.infer_hf \
+        --data_path "/home/yangx/ReasoningPathCompression/datasets/gsm8k" \
+        --output_file "eval/outputs/$MODEL_NICKNAME/profile_ca_ratio/gsm8k_n1_full.jsonl" \
+        --n_samples $N_SAMPLES \
+        --batch_size $BSZ \
+        --model_path $MODEL 
 
-# python -m eval.profile.profile_ca_ratio \
-#         --data_path "/home/yangx/ReasoningPathCompression/eval/outputs/$MODEL_NICKNAME/profile_ca_ratio/gsm8k_n1_full.jsonl" \
-#         --model $MODEL_NICKNAME --model_path $MODEL
+python -m eval.profile.profile_ca_ratio \
+        --data_path "/home/yangx/ReasoningPathCompression/eval/outputs/$MODEL_NICKNAME/profile_ca_ratio/gsm8k_n1_full.jsonl" \
+        --model $MODEL_NICKNAME --model_path $MODEL
 
 # CUDA_VISIBLE_DEVICES=${GPU_DEVICES} python -m eval.generate_answers.infer_hf \
 #         --data_path "/home/yangx/ReasoningPathCompression/datasets/MATH500" \
@@ -51,55 +51,55 @@ BSZ=1
 
 
 # BBH所有subset列表
-BBH_SUBSETS=(
-    "boolean_expressions"
-    "causal_judgement"
-    "date_understanding"
-    "disambiguation_qa"
-    "dyck_languages"
-    "formal_fallacies"
-    "geometric_shapes"
-    "hyperbaton"
-    "logical_deduction_five_objects"
-    "movie_recommendation"
-    "multistep_arithmetic_two"
-    "navigate"
-    "object_counting"
-    "penguins_in_a_table"
-    "reasoning_about_colored_objects"
-    "ruin_names"
-    "salient_translation_error_detection"
-    "snarks"
-    "sports_understanding"
-    "temporal_sequences"
-    "tracking_shuffled_objects_five_objects"
-    "web_of_lies"
-    "word_sorting"
-)
+# BBH_SUBSETS=(
+#     "boolean_expressions"
+#     "causal_judgement"
+#     "date_understanding"
+#     "disambiguation_qa"
+#     "dyck_languages"
+#     "formal_fallacies"
+#     "geometric_shapes"
+#     "hyperbaton"
+#     "logical_deduction_five_objects"
+#     "movie_recommendation"
+#     "multistep_arithmetic_two"
+#     "navigate"
+#     "object_counting"
+#     "penguins_in_a_table"
+#     "reasoning_about_colored_objects"
+#     "ruin_names"
+#     "salient_translation_error_detection"
+#     "snarks"
+#     "sports_understanding"
+#     "temporal_sequences"
+#     "tracking_shuffled_objects_five_objects"
+#     "web_of_lies"
+#     "word_sorting"
+# )
 
-# 循环处理每个subset
-for BBH_SUBSET in "${BBH_SUBSETS[@]}"; do
-    echo "Processing subset: $BBH_SUBSET"
+# # 循环处理每个subset
+# for BBH_SUBSET in "${BBH_SUBSETS[@]}"; do
+#     echo "Processing subset: $BBH_SUBSET"
     
-    CUDA_VISIBLE_DEVICES=${GPU_DEVICES} python -m eval.generate_answers.infer_hf \
-        --data_path "/home/yangx/ReasoningPathCompression/datasets/bbh" \
-        --output_file "eval/outputs/$MODEL_NICKNAME/profile_ca_ratio/bbh_n1_full/$BBH_SUBSET.jsonl" \
-        --n_samples $N_SAMPLES \
-        --batch_size $BSZ \
-        --model_path $MODEL \
-        --bbh_subset $BBH_SUBSET
+#     CUDA_VISIBLE_DEVICES=${GPU_DEVICES} python -m eval.generate_answers.infer_hf \
+#         --data_path "/home/yangx/ReasoningPathCompression/datasets/bbh" \
+#         --output_file "eval/outputs/$MODEL_NICKNAME/profile_ca_ratio/bbh_n1_full/$BBH_SUBSET.jsonl" \
+#         --n_samples $N_SAMPLES \
+#         --batch_size $BSZ \
+#         --model_path $MODEL \
+#         --bbh_subset $BBH_SUBSET
     
-    echo "Finished processing subset: $BBH_SUBSET"
-    echo "----------------------------------------"
-done
+#     echo "Finished processing subset: $BBH_SUBSET"
+#     echo "----------------------------------------"
+# done
 
-echo "All BBH subsets processing completed!"
+# echo "All BBH subsets processing completed!"
 
 
-for BBH_SUBSET in "${BBH_SUBSETS[@]}"; do
+# for BBH_SUBSET in "${BBH_SUBSETS[@]}"; do
 
-        python -m eval.profile.profile_ca_ratio \
-                --data_path "/home/yangx/ReasoningPathCompression/eval/outputs/$MODEL_NICKNAME/profile_ca_ratio/bbh_n1_full/$BBH_SUBSET.jsonl" \
-                --model $MODEL_NICKNAME --model_path $MODEL --bbh_subset $BBH_SUBSET
+#         python -m eval.profile.profile_ca_ratio \
+#                 --data_path "/home/yangx/ReasoningPathCompression/eval/outputs/$MODEL_NICKNAME/profile_ca_ratio/bbh_n1_full/$BBH_SUBSET.jsonl" \
+#                 --model $MODEL_NICKNAME --model_path $MODEL --bbh_subset $BBH_SUBSET
 
-done
+# done
--- scripts/profiling_layer_budget.sh
+++ scripts/profiling_layer_budget.sh
@@ -2,14 +2,14 @@ GPU_DEVICES=0,1,2,3
 MODEL=/home/yangx/DeepSeek-R1-Distill-Qwen-7B
 MODEL_NICKNAME=r1-7b # qwq
 
-# CUDA_VISIBLE_DEVICES=${GPU_DEVICES} python -m eval.profile.profile_layer_budget \
-#         --model_name $MODEL \
-#         --grad_dir "/home/yangx/ReasoningPathCompression/eval/profile/grad_dir/$MODEL_NICKNAME/gsm8k" \
-#         --data_path /home/yangx/ReasoningPathCompression/eval/outputs/$MODEL_NICKNAME/profile_ca_ratio/gsm8k_n1_full.jsonl 
+CUDA_VISIBLE_DEVICES=${GPU_DEVICES} python -m eval.profile.profile_layer_budget \
+        --model_name $MODEL \
+        --grad_dir "/home/yangx/ReasoningPathCompression/eval/profile/grad_dir/$MODEL_NICKNAME/gsm8k" \
+        --data_path /home/yangx/ReasoningPathCompression/eval/outputs/$MODEL_NICKNAME/profile_ca_ratio/gsm8k_n1_full.jsonl 
 
-# python -m eval.profile.draw_layer_budget \
-#         --grad_path "/home/yangx/ReasoningPathCompression/eval/profile/grad_dir/$MODEL_NICKNAME/gsm8k/grad_attn_tensor_gsm8k.pt" \
-#         --model $MODEL_NICKNAME
+python -m eval.profile.draw_layer_budget \
+        --grad_path "/home/yangx/ReasoningPathCompression/eval/profile/grad_dir/$MODEL_NICKNAME/gsm8k/grad_attn_tensor_gsm8k.pt" \
+        --model $MODEL_NICKNAME
 
 # CUDA_VISIBLE_DEVICES=${GPU_DEVICES} python -m eval.profile.profile_layer_budget \
 #         --model_name $MODEL \
@@ -44,35 +44,35 @@ MODEL_NICKNAME=r1-7b # qwq
 
 
 # BBH所有subset列表
-BBH_SUBSETS=(
-    "boolean_expressions"
-    "causal_judgement"
-    "date_understanding"
-    "disambiguation_qa"
-    "dyck_languages"
-    "formal_fallacies"
-    "geometric_shapes"
-    "hyperbaton"
-    "logical_deduction_five_objects"
-)
+# BBH_SUBSETS=(
+#     "boolean_expressions"
+#     "causal_judgement"
+#     "date_understanding"
+#     "disambiguation_qa"
+#     "dyck_languages"
+#     "formal_fallacies"
+#     "geometric_shapes"
+#     "hyperbaton"
+#     "logical_deduction_five_objects"
+# )
 
-# 循环处理每个subset
-for BBH_SUBSET in "${BBH_SUBSETS[@]}"; do
-    echo "Processing subset: $BBH_SUBSET"
-    CUDA_VISIBLE_DEVICES=${GPU_DEVICES} python -m eval.profile.profile_layer_budget \
-        --model_name $MODEL \
-        --grad_dir "/home/yangx/ReasoningPathCompression/eval/profile/grad_dir/$MODEL_NICKNAME/bbh/$BBH_SUBSET" \
-        --data_path /home/yangx/ReasoningPathCompression/eval/outputs/$MODEL_NICKNAME/profile_ca_ratio/bbh_n1_full/$BBH_SUBSET.jsonl \
-        --bbh_subset $BBH_SUBSET
+# # 循环处理每个subset
+# for BBH_SUBSET in "${BBH_SUBSETS[@]}"; do
+#     echo "Processing subset: $BBH_SUBSET"
+#     CUDA_VISIBLE_DEVICES=${GPU_DEVICES} python -m eval.profile.profile_layer_budget \
+#         --model_name $MODEL \
+#         --grad_dir "/home/yangx/ReasoningPathCompression/eval/profile/grad_dir/$MODEL_NICKNAME/bbh/$BBH_SUBSET" \
+#         --data_path /home/yangx/ReasoningPathCompression/eval/outputs/$MODEL_NICKNAME/profile_ca_ratio/bbh_n1_full/$BBH_SUBSET.jsonl \
+#         --bbh_subset $BBH_SUBSET
 
-    echo "Finished processing subset: $BBH_SUBSET"
-    echo "----------------------------------------"
+#     echo "Finished processing subset: $BBH_SUBSET"
+#     echo "----------------------------------------"
 
-    python -m eval.profile.draw_layer_budget \
-        --grad_path "/home/yangx/ReasoningPathCompression/eval/profile/grad_dir/$MODEL_NICKNAME/bbh/$BBH_SUBSET/grad_attn_tensor_bbh.pt" \
-        --model $MODEL_NICKNAME \
-        --bbh_subset $BBH_SUBSET
+#     python -m eval.profile.draw_layer_budget \
+#         --grad_path "/home/yangx/ReasoningPathCompression/eval/profile/grad_dir/$MODEL_NICKNAME/bbh/$BBH_SUBSET/grad_attn_tensor_bbh.pt" \
+#         --model $MODEL_NICKNAME \
+#         --bbh_subset $BBH_SUBSET
 
-done
+# done
 
-echo "All BBH subsets processing completed!"
+# echo "All BBH subsets processing completed!"
